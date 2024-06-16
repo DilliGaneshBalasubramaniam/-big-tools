0670896d39432cf93ae49ecc63871881b38b88a9
DilliGanesh Balasubramainam <dilliganesh111998@gmail.com>
2024-06-17 00:15:01 +0530

    added the vvalues

---
+++ datatypes python.py
@@ -0,0 +1,90 @@
+# string - raw text
+"this is also a string" # use double quote string
+'this is string' # use single quote string
+#multiple line quatation
+""""
+ncnndmnmdcn
+dmcm,mc,m,cm
+knckkckmc
+"""          
+'''
+ndjncjncjn
+jnjnmnk
+kmkmkmk
+'''
+# integer - data type-23
+#float -23.6
+#complex data type -2j+3
+#range data type - range(0,100)
+#boolean data type -True or false
+# list data type -["apple","orange","grapes"]
+#tuple data type -("apple","orange","grapes")
+#set data type -{"apple","orange","grapes"}
+#dictionary data type -{"fruit1":"apple","fruit2":"orange"}
+
+
+
+# operators
+# + - * /
+# % - modulus
+# // - floor division
+
+# print(2+5)
+
+# print(2-5)
+
+# print(2%5)
+print(5%2)
+
+# print(2//5)
+# -----------------------------------------------
+# =, ==,>,<,>=,<=
+# print(24==23)
+# print(24<12)
+# print(23<=23)
+# print(76>=23)
+# print(45>56)
+# print(57>=56)
+
+#------------------------------------------------
+# +=,-=,*=,/=
+
+# x=10
+# x=x+1
+# print(x) 
+
+# x=10
+# x += 1
+# print(x)
+
+# x=10
+# x-=1
+# print(x) 
+
+# x= 20
+# x /= 1 
+# print(x)
+
+#-------------------------------------------
+# AND, OR ,NOT
+
+# print(24<25 and 24==56)
+# print(21<65 or 25>=32)
+# print(21<65 or 25>=32)
+# print(21<65 or 25>=32)
+# print(not(21<65 or 25>=32))
+# print(24==12 or 24<12)
+
+#-----------------------------------------------
+
+# is, isnot
+# print('dilli' is 'l')
+# print(not('dilli' is 'l'))
+
+# #------------------------------------------------
+
+# # in,notin
+
+# print('don'in 'g')
+# print(not("don") in "g")
+       
